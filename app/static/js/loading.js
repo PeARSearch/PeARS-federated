183002e39d19ec677b830b0321829bdff81376dc
minimalparts <aurelie.herbelot@cantab.net>
2024-01-26 11:19:52 +0100

    Removed list and bookmarks indexing from interface

--- app/static/js/loading.js
+++ app/static/js/loading.js
@@ -5,20 +5,5 @@
 document.getElementById('searchbutton').addEventListener('click', function(e) {
   document.getElementById("searchbutton").style.display='none';
   document.getElementById("loadingbutton").style.display='block';
-  document.getElementById("searchform").submit();
 });
 
-document.getElementById('indexurlbutton').addEventListener('click', function(e) {
-  document.getElementById("indexurlbutton").style.display='none';
-  document.getElementById("loadingurlbutton").style.display='block';
-});
-
-document.getElementById('indexfilebutton').addEventListener('click', function(e) {
-  document.getElementById("indexfilebutton").style.display='none';
-  document.getElementById("loadingfilebutton").style.display='block';
-});
-
-document.getElementById('indexbookmarksbutton').addEventListener('click', function(e) {
-  document.getElementById("indexbookmarksbutton").style.display='none';
-  document.getElementById("loadingbookmarksbutton").style.display='block';
-});
