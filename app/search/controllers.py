b2d1ddd2f67823271c695ff69e9a9080afa90e5e
minimalparts <aurelie.herbelot@cantab.net>
2024-03-04 11:12:01 +0100

    Updated search module for concurrency case

--- app/search/controllers.py
+++ app/search/controllers.py
@@ -1,27 +1,12 @@
-# SPDX-FileCopyrightText: 2022 PeARS Project, <community@pearsproject.org>, 
+# SPDX-FileCopyrightText: 2024 PeARS Project, <community@pearsproject.org>, 
 #
 # SPDX-License-Identifier: AGPL-3.0-only
 
-# Import flask dependencies
-from flask import Blueprint, request, render_template, send_from_directory
-from flask import current_app
-
-# Import the database object from the main app module
-from app.api.models import Urls
+from os.path import dirname, join, realpath
+from flask import Blueprint, request, render_template
 from app.search import score_pages
-
-# Import matrix manipulation modules
-import numpy as np
-from scipy import sparse
-
-# Import utilities
-import re
-import logging
-from os.path import dirname, join, realpath, isfile
-from app.utils import init_podsum, beautify_title, beautify_snippet
+from app.utils import beautify_title, beautify_snippet
 from app import EXPERT_ADD_ON, OWN_BRAND, WALKTHROUGH, STOPWORDS
-
-LOG = logging.getLogger(__name__)
 
 # Define the blueprint:
 search = Blueprint('search', __name__, url_prefix='')
@@ -33,55 +18,41 @@ pod_dir = join(dir_path,'app','static','pods')
 
 @search.context_processor
 def inject_brand():
+    """Inject brand information into page
+    (logo on all pages and info on start page.)
+    """
     return dict(own_brand=OWN_BRAND)
 
 @search.route('/')
 @search.route('/index')
-def index():  
+def index():
+    """ Route for the main search page.
+    """
     results = []
     internal_message = ""
-    if Urls.query.count() == 0:
-        init_podsum()
-
     query = request.args.get('q')
     if not query:
         if OWN_BRAND:
-            with open(join(static_dir,'intro.txt'), 'r') as file:
-                internal_message = file.read().replace('\n', '<br>')
-        return render_template("search/index.html", internal_message=internal_message, own_brand=OWN_BRAND)
-    else:
-        displayresults = []
-        query = ' '.join([w for w in query.split() if w not in STOPWORDS])
-        if WALKTHROUGH:
-            with open(join(static_dir,'walkthrough.txt'), 'r') as file:
-                internal_message = file.read().replace('\n', '<br>')
-        results = score_pages.run_search(query)
-        if not results:
-            results = None
-            return render_template('search/results.html', query=query, results=results, own_brand=OWN_BRAND)
-        for r in results:
-            if r['doctype'] != 'csv' and r['doctype'] != 'doc':
-                r['snippet'] = ' '.join(r['snippet'].split()[:11]) #11 to conform with EU regulations
-            r['title'] = beautify_title(r['title'], r['doctype'])
-            r['snippet'] = beautify_snippet(r['snippet'], r['img'], query, EXPERT_ADD_ON)
-            displayresults.append(list(r.values()))
-        query = query.replace(' ','&nbsp;')
-        return render_template('search/results.html', query=query, results=displayresults, internal_message=internal_message, expert=EXPERT_ADD_ON, own_brand=OWN_BRAND)
-
-@search.route('/experts/<kwd>/<idx>/')
-def experts(kwd,idx):  
-    print(idx,kwd)
+            with open(join(static_dir,'intro.txt'), 'r', encoding="utf-8") as f:
+                internal_message = f.read().replace('\n', '<br>')
+        return render_template("search/index.html", internal_message=internal_message, \
+                own_brand=OWN_BRAND)
     displayresults = []
-    results, pods = score_pages.score_experts(idx,kwd)
+    query = ' '.join([w for w in query.split() if w not in STOPWORDS])
+    if WALKTHROUGH:
+        with open(join(static_dir,'walkthrough.txt'), 'r', encoding="utf-8") as f:
+            internal_message = f.read().replace('\n', '<br>')
+    results = score_pages.run_search(query)
     if not results:
-        pears = ['no pear found :(']
-        results = [{'url':None, 'title':None, 'snippet':'No pages found', 'doctype':None, 'notes':None, 'img':None, 'trigger':None, 'contributor':None}]
+        results = None
+        return render_template('search/results.html', \
+                query=query, results=results, own_brand=OWN_BRAND)
     for r in results:
+        if r['doctype'] != 'csv' and r['doctype'] != 'doc':
+            r['snippet'] = ' '.join(r['snippet'].split()[:11]) #11 to conform with EU regulations
         r['title'] = beautify_title(r['title'], r['doctype'])
-        r['snippet'] = beautify_snippet(r['snippet'], r['img'], 'ind', EXPERT_ADD_ON)
+        r['snippet'] = beautify_snippet(r['snippet'], r['img'], query, EXPERT_ADD_ON)
         displayresults.append(list(r.values()))
-    return render_template('search/results.html', pears=[], query="-", results=displayresults, expert=EXPERT_ADD_ON, own_brand=OWN_BRAND)
-
-@search.route('/html_cache/<path:filename>')
-def custom_static(filename):
-    return send_from_directory('html_cache', filename)
+    query = query.replace(' ','&nbsp;')
+    return render_template('search/results.html', query=query, results=displayresults, \
+            internal_message=internal_message, expert=EXPERT_ADD_ON, own_brand=OWN_BRAND)
