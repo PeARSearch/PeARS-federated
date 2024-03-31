a98145f2d1c715b389eea98f4c578b7edeaf4d05
minimalparts <aurelie.herbelot@cantab.net>
2024-03-31 14:00:04 +0200

    Fixed URLs for local content in orchard

--- app/orchard/mk_urls_file.py
+++ app/orchard/mk_urls_file.py
@@ -2,12 +2,9 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-only
 
+from os.path import dirname, realpath, join
+from flask import request
 from app.api.models import Urls
-from app import db
-from os.path import dirname, realpath, join, basename
-import numpy as np
-from scipy.sparse import vstack, load_npz
-from collections import Counter
 
 dir_path = dirname(dirname(realpath(__file__)))
 pod_dir = join(dir_path,'static','pods')
@@ -17,12 +14,17 @@ def get_url_list_for_users(theme):
     urls = []
     url_theme = theme.replace(' ', '_')
     hfile = join(dir_path, "static", "pods", url_theme + ".pears.txt")
-    f_out = open(hfile,'w')
+    f_out = open(hfile,'w', encoding='utf-8')
     for url in Urls.query.filter(Urls.pod.contains(theme+'.u.')).all():
         if not url.pod.startswith(theme+'.u.'):
             continue
-        urls.append(url.url)
-        f_out.write(url.url+'\n')
+        if url.url.startswith('pearslocal'):
+            url = join(request.host_url,'api','get?url='+url.url)
+            urls.append(url)
+            f_out.write(url+'\n')
+        else:
+            urls.append(url.url)
+            f_out.write(url.url+'\n')
     f_out.close()
     filename = hfile.split('/')[-1]
     return filename, urls
