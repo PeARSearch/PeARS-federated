38b208abc9c360c7a8de9306b6dc9c4fd74f607f
minimalparts <aurelie.herbelot@cantab.net>
2024-04-06 21:17:13 +0200

    Some gettext added

--- app/auth/decorators.py
+++ app/auth/decorators.py
@@ -5,7 +5,7 @@ def check_is_confirmed(func):
     @wraps(func)
     def decorated_function(*args, **kwargs):
         if current_user.is_confirmed is False:
-            flash("Please confirm your account!", "warning")
+            flash(gettext("Please confirm your account!"), "warning")
             return redirect(url_for("accounts.inactive"))
         return func(*args, **kwargs)
 
