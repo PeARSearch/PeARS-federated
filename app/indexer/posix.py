a11bd76d4860986e7e9edec11513556537e4c805
Nandaja Varma <nandaja.varma@gmail.com>
2024-06-24 16:59:18 +0000

    Use the right pod directory in mk_urls_file

--- app/indexer/posix.py
+++ app/indexer/posix.py
@@ -4,7 +4,7 @@ from os.path import join, dirname, realpath
 from app import models
     
 dir_path = dirname(dirname(realpath(__file__)))
-pod_dir = join(dir_path,'pods')
+pod_dir = getenv("PODS_DIR", join(dir_path, 'pods'))
 
 def load_posix(contributor, lang, theme):
     posix_path = join(dir_path, 'pods', contributor, lang)
