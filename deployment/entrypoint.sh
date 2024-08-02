4d3f6b3dc7efdd357af1ba038e017924088a4416
Nandaja Varma <nandaja.varma@gmail.com>
2024-08-02 06:59:20 +0000

    Run the install-language in entrypoint

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 # entrypoint.sh
 
+# Extract the languages from the environment variable
+IFS=',' read -ra LANGS <<< "${PEARS_LANGS}"
+
+# Loop through each language and install it unless it's 'eng'
+for lang in "${LANGS[@]}"; do
+  if [ "$lang" != "en" ]; then
+    flask pears install-language "$lang"
+  fi
+done
+
 flask db migrate
 
 gunicorn -b 0.0.0.0:8000 -w 3 -t 120 app:app
