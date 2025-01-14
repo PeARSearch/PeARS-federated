6cbbaf442a5c8f6ca598023fa9d5b58f034de5c3
Gosse Minnema <gosseminnema@gmail.com>
2025-01-14 14:47:27 +0100

    remove redundant comment

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 # dictionary mapping view function names to information about access requirements
-# the dictionary is compiled at runtime from the login_required(!!!), check_is_confirmed, check_is_admin decorators
+# the dictionary is compiled at runtime from the check_permissions decorator
 # solution inspired by https://stackoverflow.com/a/34664604
 # example: {"indexer.index"}
 
