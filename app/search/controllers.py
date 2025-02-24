9d6919bbc72672098146fe6e6787ceadbf320dbd
Gosse Minnema <gosseminnema@gmail.com>
2025-02-24 22:16:13 +0100

    Sitename should match host name, tell user this and give warning at startup if it mismatches

--- app/search/controllers.py
+++ app/search/controllers.py
@@ -81,7 +81,7 @@ def prepare_gui_results(query, results):
         sitename = app.config['SITENAME']
         # results from our own instance
         our_host_url = request.host_url
-        if r['share'].startswith(our_host_url):
+        if r['share'].startswith(sitename):
             r['instance'] = sitename
             r['instance_is_local'] = True
             r['instance_info_text'] = gettext("This result originates from the local PeARS instance.")
