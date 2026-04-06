3c5000d8e0ff0d27daaa00d72bae99683db42c8f
minimalparts <aurelie.herbelot@cantab.net>
2026-04-06 10:50:44 +0200

    Fixed default sitename

--- app/init_config.py
+++ app/init_config.py
@@ -13,7 +13,7 @@ def run_config(app):
     app.config['MAIL_DEBUG'] = False
     app.config['MAIL_USERNAME'] = getenv("EMAIL_USER")
     app.config['MAIL_PASSWORD'] = getenv("EMAIL_PASSWORD")
-    app.config['SITENAME'] = getenv("SITENAME")
+    app.config['SITENAME'] = getenv("SITENAME", "local")
     app.config['SITE_TOPIC'] = getenv("SITE_TOPIC")
     app.config['SEARCH_PLACEHOLDER'] = getenv("SEARCH_PLACEHOLDER")
     app.config['SQLALCHEMY_DATABASE_URI'] = getenv("SQLALCHEMY_DATABASE_URI", app.config.get("SQLALCHEMY_DATABASE_URI"))
